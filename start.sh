ad84a9b6cd15eeec221aa5bc614a348104f73bf7
Kaviru55 <111237723+Kaviru55@users.noreply.github.com>
2023-02-11 16:03:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
 docker login --username kaviru2007 --password slofficial
 docker run -p 8080:8080 kaviru2007/mirror-bot
